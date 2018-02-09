eedf8d55fd49eb6bd58a2861ed9a46d0394a4afa
Bartłomiej Płoszyński <b.ploszynski@gmail.com>
2018-02-09 21:15:31 +0100

    Dodanie usuwania rekordu <li> po naciśnięciu X.

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,3 +1,14 @@
+//dodajemy toggle po naciscnicu li
 $("li").click(function(){
 	$(this).toggleClass('selected');
-})
+});
+
+//Click on X delete Todo
+$("span").click(function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	//Zatrzymujemy propagacje click listenerów
+	event.stopPropagation();
+
+});
